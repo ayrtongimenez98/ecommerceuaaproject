c0cba9eb21e2d4ff17f16ec9c4cbd12615c63c74
Ayrton Giménez <ayrtondavalos98@outlook.com>
2019-10-12 11:51:18 -0300

    first commit

---
+++ UAAEcommerce/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UAAEcommerce.MvcApplication" Language="C#" %>
---
+++ UAAEcommerce/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general sobre un ensamblado se controla mediante lo siguiente
+// conjunto de atributos. Cambie los valores de estos atributos para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("UAAEcommerce")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UAAEcommerce")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si configura ComVisible como falso, los tipos de este ensamblado no se hacen visibles
+// para componentes COM.  Si necesita acceder a un tipo de este ensamblado desde
+// COM, establezca el atributo ComVisible en True en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como ID de typelib si este proyecto se expone a COM
+[assembly: Guid("0921f56d-642d-470e-8499-11953814da39")]
+
+// La información de versión de un ensamblado consta de los siguientes cuatro valores:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o puede predeterminar los números de compilación y de revisión
+// mediante el carácter '*', como se muestra a continuación:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
