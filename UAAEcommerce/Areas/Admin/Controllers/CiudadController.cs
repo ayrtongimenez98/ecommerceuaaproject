9472b2a206ffb20f76b5e927b7ba1b51df4ee001
Gustavo Talavera <48735420+gustalavera@users.noreply.github.com>
2019-10-16 20:06:06 -0300

    Tabla y formulario ciudad

---
+++ UAAEcommerce/Areas/Admin/Controllers/CiudadController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using UAAEcommerce.Areas.Admin.Models;
+
+namespace UAAEcommerce.Areas.Admin.Controllers
+{
+    public class CiudadController : Controller
+    {
+        private UAAEcommerceEntities db = new UAAEcommerceEntities();
+
+        // GET: Admin/Ciudad
+        public ActionResult Index()
+        {
+            return View(db.Ciudad.ToList());
+        }
+
+        // GET: Admin/Ciudad/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ciudad ciudad = db.Ciudad.Find(id);
+            if (ciudad == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ciudad);
+        }
+
+        // GET: Admin/Ciudad/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Ciudad/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idCiudad,ciu_descripcion")] Ciudad ciudad)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Ciudad.Add(ciudad);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(ciudad);
+        }
+
+        // GET: Admin/Ciudad/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ciudad ciudad = db.Ciudad.Find(id);
+            if (ciudad == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ciudad);
+        }
+
+        // POST: Admin/Ciudad/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idCiudad,ciu_descripcion")] Ciudad ciudad)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(ciudad).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(ciudad);
+        }
+
+        // GET: Admin/Ciudad/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ciudad ciudad = db.Ciudad.Find(id);
+            if (ciudad == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ciudad);
+        }
+
+        // POST: Admin/Ciudad/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Ciudad ciudad = db.Ciudad.Find(id);
+            db.Ciudad.Remove(ciudad);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
